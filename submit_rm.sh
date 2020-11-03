476852e3ea6917d34ee21cdee7d698a3a2c3bd6f
Martin Huber <mhubii@users.noreply.github.com>
2020-11-03 16:15:03 +0000

    Delete submit_rm.sh

--- submit_rm.sh
+++
@@ -1,6 +0,0 @@
-runai submit toy-runai \
-  -p mhuber \
-  -i python \
-  -v /nfs/home/mhuber/proj/toy_runai/:/workspace/toy_runai \
-  --command /workspace/toy_runai/run_rm.sh \
-  --working-dir /workspace/toy_runai/ 
